69b5cc5a0cbc80dd4dc8026405f185557cfdcbc0
jadebustos <jadebustos@gmail.com>
2016-11-16 21:06:43 +0100

    examples to compile with static library

--- biagra/examples/datamatrix-biaMatrix.c
+++ biagra/examples/datamatrix-biaMatrix.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-#include "datamatrix.h"
+#include <biagra/datamatrix.h>
 
 /*                                                                      */
 /*      B.I.A.G.R.A.    (c) 1998 Jose Angel de Bustos Perez             */
