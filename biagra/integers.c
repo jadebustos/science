28c4e95ea4871fb06b944d379f00b44ee089dac5
jadebustos <jadebustos@gmail.com>
2016-11-20 00:18:36 +0100

    integers stuff

--- biagra/integers.c
+++ biagra/integers.c
@@ -1,82 +1,47 @@
-#include <math.h>
+#include <stdlib.h>
 
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
-/*									*/
-/*	B.I.A.G.R.A.	(c) 1998 Jose Angel de Bustos Perez		*/
-/*			 <jadebustos@gmail.com>         		*/
-/*									*/
+/*                                                                      */
+/*      B.I.A.G.R.A.    (c) 1998 Jose Angel de Bustos Perez             */
+/*                           <jadebustos@gmail.com>                     */
+/*                                                                      */
 /*      This software is licensed under GPLv2:                          */
 /*        http://www.gnu.org/licenses/gpl-2.0.html                      */
-/*									*/
-/*	BIbliotecA de proGRamacion cientificA.				*/
-/*									*/
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
+/*                                                                      */
+/*      BIbliotecA de proGRamacion cientificA.                          */
+/*                                                                      */
 
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
 /*                                                                      */
-/* enteros.c    Jose Angel de Bustos Perez                              */
-/*                                                                      */
-/* Funciones para el tratamiento de matrices.                           */
-/*                                                                      */
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
-
-
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
-/*                                                                      */
-/* IMPORTANTE!!!                                                        */
-/*                                                                      */
-/*     1) El autor no se responsabiliza de los posibles bugs(si los     */
-/*        hubiera) ni del mal uso de esta biblioteca.                   */
-/*                                                                      */
-/*     2) Esta biblioteca ha sido desarrollada y testeada bajo LiNUX.   */
+/* Function to sum the first n integers                                 */
 /*                                                                      */ 
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
-
-
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
+/* The forllowing codes are returned:                                   */
 /*                                                                      */
-/* Funcion que calcula y devuelve la suma de los intNumero primeros     */
-/* enteros.                                                             */
+/*      0 If sum is bigger than unsigned int maximum value              */
 /*                                                                      */
-/* La funcion devuelve los siguientes codigos:                          */
-/*                                                                      */
-/*      0 Si el resultado de la suma excede el rango de un dato del     */
-/*        tipo int.                                                     */
-/*                                                                      */
-/*      Un numero distinto de cero que sera la suma buscada.            */
+/*      Sum of the first n integers                                     */
 /*                                                                      */ 
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
 
-unsigned SumaNumerosN(int intNumero)
+unsigned uintSumFirstNIntegers(int n) {
+	
+  double dblHalf = .0,
+	 dblSum  = .0;
 
-{
-double 	dblMitad = .0,
-	dblSuma  = .0;
+  int intNum = abs(n);
 
-int     intNum = abs(intNumero);
+  unsigned intSum = 0;
 
-unsigned intSuma = 0;
+  if ( (intNum % 2) == 0 ) {
+    dblHalf = ((double)intNum)/2.;
+    dblSum  = dblHalf*(1.+(double)intNum);
+  }
+  else {
+    dblHalf = (((double)intNum)-1.)/2.;
+    dblSum  = (dblHalf)*(2.+((double)intNum))+1.;
+  }
 
-if ( (intNum % 2) == 0 ) /* intNumero ES PAR */
+  intSum = (unsigned)dblSum;
 
-	{
-	dblMitad = ((double)intNum)/2.;
-
-	dblSuma = dblMitad*(1.+(double)intNum);
-	}
-
-else /* intNumero ES IMPAR */
-
-	{
-	dblMitad = (((double)intNum)-1.)/2.;
-
-	dblSuma = (dblMitad)*(2.+((double)intNum))+1.;
-	}
-
-intSuma = (unsigned)dblSuma;	/* CONVERTIMOS LA SUMA A int */
-
-if ( dblSuma > intSuma ) /* EL RESULTADO EXCEDE EL RANGO DEL unsigned */
-	intSuma = 0;
+  if ( dblHalf > intSum )
+    intSum = 0;
 		
-return (intSuma);
+  return (intSum);
 }
--- biagra/integers.h
+++ biagra/integers.h
@@ -1,41 +1,19 @@
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
-/*									*/
-/*	B.I.A.G.R.A.	(c) 1998 Jose Angel de Bustos Perez		*/
-/*			 <jadebustos@gmail.com>         		*/
-/*									*/
+#ifndef _INTEGERS_H
+  #define _INTEGERS_H
+/*                                                                      */
+/*      B.I.A.G.R.A.    (c) 1998 Jose Angel de Bustos Perez             */
+/*                           <jadebustos@gmail.com>                     */
+/*                                                                      */
 /*      This software is licensed under GPLv2:                          */
 /*        http://www.gnu.org/licenses/gpl-2.0.html                      */
-/*									*/
-/*	BIbliotecA de proGRamacion cientificA.				*/
-/*									*/
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
+/*                                                                      */
+/*      BIbliotecA de proGRamacion cientificA.                          */
+/*                                                                      */
 
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
 /*                                                                      */
-/* enteros.h    Jose Angel de Bustos Perez                              */
-/*                                                                      */
-/* Funciones para el tratamiento de matrices.                           */
-/*                                                                      */
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
-
-
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
-/*                                                                      */
-/* IMPORTANTE!!!                                                        */
-/*                                                                      */
-/*     1) El autor no se responsabiliza de los posibles bugs(si los     */
-/*        hubiera) ni del mal uso de esta biblioteca.                   */
-/*                                                                      */
-/*     2) Esta biblioteca ha sido desarrollada y testeada bajo LiNUX.   */
+/* Function to sum the first n integers                                 */
 /*                                                                      */ 
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
 
+unsigned uintSumFirstNIntegers(int n);
 
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
-/*                                                                      */
-/* Funcion que calcula y devuelve la suma de los intNumero primeros     */
-/* enteros.                                                             */
-/*                                                                      */ 
-/*-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_*/
-
-unsigned SumaNumerosN(int intNumero);
+#endif
